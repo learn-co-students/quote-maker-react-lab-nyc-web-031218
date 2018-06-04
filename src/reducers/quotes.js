ad7711c57c13cb390e683cd53ef195236d3728b9
victoria-huang <vctrah@gmail.com>
2018-06-04 13:19:06 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,35 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      const q = {...action.quote, id: uuid(), votes: 0};
+      // console.log(state)
+      return state.concat(q);
+    case 'REMOVE_QUOTE':
+      // const quotes = state.filter(quote => quote.id !== action.id);
+      return state.filter(quote => quote.id !== action.quoteId);
+    case 'UPVOTE_QUOTE':
+      let quote = state.find(quote => quote.id === action.quoteId);
+      // console.log(quote)
+      let newQuote = {...quote, votes: quote.votes + 1}
+      let quoteIdx = state.findIndex(quote => quote.id === action.quoteId);
+      let stateCopy = state.slice();
+      stateCopy[quoteIdx] = newQuote
+      return stateCopy;
+    case 'DOWNVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId);
+      // console.log(quote)
+      if (quote.votes > 0) {
+        newQuote = {...quote, votes: quote.votes - 1}
+        quoteIdx = state.findIndex(quote => quote.id === action.quoteId);
+        stateCopy = state.slice();
+        stateCopy[quoteIdx] = newQuote
+        return stateCopy;
+      } else {
+        return state;
+      }
+    default:
+      return state;
+  }
 }
